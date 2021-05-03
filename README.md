22793dc988f6298140f8164270c8de8ca9e6a6d2
cbucquet <charles@bucquet.com>
2021-05-03 12:10:02 -0700

    Added solution in main.js

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # kg_cbucquet_2021
 Kargo online assessment
+
+Converts an array of integers into an array of string that are the phonetic equivalent
+Ex:
+10 2 53 -> OneZero,Two,FiveThree
+1 100 28 -> One,OneZeroZero,TwoEight
--- main.js
+++ main.js
@@ -1,2 +1,42 @@
 // Kargo online cosing assessement
 
+
+// From a digit return the corresponding string
+function digitToString(d) {
+    switch(d) {
+        case '0':
+            return "Zero";
+        case '1':
+            return "One";
+        case '2':
+            return "Two";
+        case '3':
+            return "Three";
+        case '4':
+            return "Four";
+        case '5':
+            return "Five";
+        case '6':
+            return "Six";
+        case '7':
+            return "Seven";
+        case '8':
+            return "Eight";
+        case '9':
+            return "Nine";
+        default:
+            return "None";
+    }
+}
+
+// Loop through the inputs and write out each number
+var length = process.argv.length;
+for(var i = 2; i < length; i++){
+    var number = process.argv[i];
+    for(var j = 0; j < number.length; j++){
+        process.stdout.write(digitToString(number.charAt(j)));
+    }
+    if(i+1 < length){
+        process.stdout.write(',');
+    }
+}
